c5d6cf59111d049d79db180367a445fc860096d7
calidion <calidion@gmail.com>
2019-04-02 18:07:07 +0800

    enabled parse wallet

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,67 @@
-use chrono::{NaiveDate, NaiveDateTime};
-use chrono::{TimeZone, Utc};
+use chrono::NaiveDateTime;
 use cryptonote_account::{Account, Address};
-use cryptonote_raw_crypto::{Chacha, ChachaIV, ChachaKey};
 use cryptonote_currency::Currency;
+use cryptonote_raw_crypto::{Chacha, ChachaIV, ChachaKey};
 use cryptonote_varint as varint;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Read, Write};
-use std::rc::Rc;
-use ed25519_dalek::{PublicKey, SecretKey, Keypair};
+// use std::rc::Rc;
+use ed25519_dalek::{Keypair, PublicKey, SecretKey};
 
+type Keys = ([u8; 32], [u8; 32]);
 pub struct Wallet {
-  account: Option<Account>,
-  currency: Currency,
+  version: Option<u64>,
+  spend_keys: Option<Keys>,
+  view_keys: Option<Keys>,
+  createtime: Option<u64>,
+  iv: Option<[u8; 8]>,
+  password: String,
+  loaded: bool
 }
 
 impl Wallet {
-  pub fn new(currency: Currency) -> Wallet {
+  pub fn new() -> Wallet {
     Wallet {
-      account: None,
-      currency,
+      version: None,
+      spend_keys: None,
+      view_keys: None,
+      createtime: None,
+      iv: None,
+      password: String::from(""),
+      loaded: false
     }
   }
 
-  pub fn parseKey(r: & mut Read) -> [u8; 32]{
+  pub fn parse_key(r: &mut Read) -> [u8; 32] {
     let mut key: [u8; 32] = [0; 32];
     r.read_exact(&mut key).unwrap();
     key
   }
 
-  pub fn prase(buffer: &[u8]) {
+  fn match_keys(&self, ik: &Option<Keys>) -> [u8; 32] {
+    let mut k: [u8; 32] = [0; 32];
+    match ik {
+      Some(keys) => {
+        k = keys.0;
+        Some(keys)
+      }
+      None => None,
+    };
+    k
+  }
+
+  pub fn to_address(&self) -> Address {
+    let spend_pubk: PublicKey = PublicKey::from_bytes(&self.match_keys(&self.spend_keys)).unwrap();
+    let view_pubk: PublicKey = PublicKey::from_bytes(&self.match_keys(&self.view_keys)).unwrap();
+    let address = Address::new(0x3d, spend_pubk, view_pubk);
+    println!("{}", address.get());
+    address
+  }
+
+  pub fn prase(&mut self, buffer: &[u8]) {
     let mut buffered = BufReader::new(buffer);
-    let createtime: u64 = varint::read(& mut buffered);
+    let createtime: u64 = varint::read(&mut buffered);
+    self.createtime = Some(createtime);
     println!("{}", createtime);
 
     let datetime = NaiveDateTime::from_timestamp(createtime as i64, 0);
@@ -39,34 +70,39 @@ impl Wallet {
     println!("{}", createtime);
     println!("{}", datetime.format("%Y-%m-%d %H:%M:%S"));
 
-    let spend_public_key = Wallet::parseKey(& mut buffered);
-    let spend_private_key = Wallet::parseKey(& mut buffered);
-    let view_public_key = Wallet::parseKey(& mut buffered);
-    let view_private_key = Wallet::parseKey(& mut buffered);
+    let spend_public_key = Wallet::parse_key(&mut buffered);
+    let spend_private_key = Wallet::parse_key(&mut buffered);
+    let view_public_key = Wallet::parse_key(&mut buffered);
+    let view_private_key = Wallet::parse_key(&mut buffered);
+
+    self.spend_keys = Some((spend_private_key, spend_public_key));
+    self.view_keys = Some((view_private_key, view_public_key));
     println!("{:?}", spend_public_key);
     println!("{:?}", spend_private_key);
     println!("{:?}", view_public_key);
     println!("{:?}", view_private_key);
-    let spend_prik: SecretKey = SecretKey::from_bytes(&spend_private_key).unwrap();
-    let view_prik: SecretKey = SecretKey::from_bytes(&view_private_key).unwrap();
-    let spend_pubk: PublicKey = PublicKey::from_bytes(&spend_public_key).unwrap();
-    let view_pubk: PublicKey = PublicKey::from_bytes(&view_public_key).unwrap();
+    // let spend_prik: SecretKey = SecretKey::from_bytes(&spend_private_key).unwrap();
+    // let view_prik: SecretKey = SecretKey::from_bytes(&view_private_key).unwrap();
+    // let spend_pubk: PublicKey = PublicKey::from_bytes(&spend_public_key).unwrap();
+    // let view_pubk: PublicKey = PublicKey::from_bytes(&view_public_key).unwrap();
     // let into_spend_pubk: PublicKey = (&spend_prik).into();
     // let into_view_pubk: PublicKey = (&view_prik).into();
     // assert!(into_spend_pubk.to_bytes() == spend_public_key);
     // assert!(into_view_pubk.to_bytes() == view_public_key);
-    let address = Address::new(0x3d, spend_pubk, view_pubk);
-    println!("{}", address.get());
+    // let address = Address::new(0x3d, spend_pubk, view_pubk);
+    // println!("{}", address.get());
   }
 
-  pub fn load(file: String, password: &str) {
+  pub fn load(&mut self, file: String, password: &str) {
     let input = File::open(file).expect("File not found!");
     let mut buffered = BufReader::new(input);
     let version: u64 = varint::read(&mut buffered);
+    self.version = Some(version);
     let mut iv: [u8; 8] = [0; 8];
     buffered
       .read_exact(&mut iv[..])
       .expect("Failed to read iv!");
+    self.iv = Some(iv);
     let cipher_len: u64 = varint::read(&mut buffered);
     println!("version is : {}", version);
     println!("iv is : {:?}", iv);
@@ -76,7 +112,6 @@ impl Wallet {
     buffered
       .read_exact(&mut cipher[..])
       .expect("Failed to read cipher!");
-
 
     println!("password is : {}", password);
 
@@ -91,17 +126,26 @@ impl Wallet {
     println!("dec is : {:x?}", &dec[0]);
     println!("dec is : {:x?}", &dec[dec.len() - 1]);
 
-    Wallet::prase(&dec);
+    self.prase(&dec);
+    self.password = String::from(password);
+    self.loaded = true;
   }
+
+  pub fn save(&self, file: String) {}
 }
 
 #[cfg(test)]
 mod tests {
   use super::Wallet;
+
   #[test]
 
   fn should_read() {
-    Wallet::load(String::from("tests/vig.wallet"), "");
-    Wallet::load(String::from("tests/vig-enc.wallet"), "abcd$1234");
+    let mut wallet = Wallet::new();
+    wallet.load(String::from("tests/vig.wallet"), "");
+    let mut wallet = Wallet::new();
+    wallet.load(String::from("tests/vig-enc.wallet"), "abcd$1234");
+    // let mut wallet = Wallet::new();
+    // wallet.load(String::from("tests/pool.wallet"), "abcd$1234");
   }
 }
